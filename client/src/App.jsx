5161888444664f9045586c9f599ae6ea791bf003
Maria Padilla <maria.pad.mar@gmail.com>
2024-02-20 17:54:11 +0100

    Style front page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -44,29 +44,29 @@ function App() {
   // }
 
   return (
-    <>
-      <h1>Find your favourite plants</h1>
+    <div className='container'>
+      <h2 className='display-3 mb-4'>Find your favourite plants</h2>
       <form >
         <BooleanFilter value={"indoor"} setParams={setParams} params={params} />
         <BooleanFilter value={"edible"} setParams={setParams} params={params}/>
         <BooleanFilter value={"poisonous"} setParams={setParams} params={params}/>         
-        <input onChange={(e) => setParams({...params, "query": e.target.value})} placeholder="Search by name" name="search-bar"></input>
-        <button onClick={clearParams}>Clear</button>
-        <button>Search</button>
+        <input onChange={(e) => setParams({...params, "query": e.target.value})} placeholder="Search by name" name="search-bar" className='form-control m-3' ></input>
+        <button onClick={clearParams} className='btn btn-light me-2 mb-4'>Clear</button>
+        <button className='btn btn-success mb-4'>Search</button>
       </form>
 
       <div>{error}</div>
 
-      <div>
+      <div className='row'>
         {plants.map(plant => 
-          <div key={plant.id}>
+          <div key={plant.id} className='col' >
             <p>{plant.scientific_name}</p>
             {/* https://stackoverflow.com/questions/40108298/react-get-object-inside-another-object */}
-            <img src={plant.default_image && plant.default_image.thumbnail ? plant.default_image.thumbnail : null}/>
+            <img src={plant.default_image && plant.default_image.thumbnail ? plant.default_image.thumbnail : null} className='rounded mb-5'/>
           </div>)}
       </div>
 
-    </>
+    </div>
   )
 }
 
--- client/src/components/BooleanFilter.jsx
+++ client/src/components/BooleanFilter.jsx
@@ -4,9 +4,9 @@ export default function BooleanFilter({value, setParams, params}) {
 
   return (
     <div>
-        <label>{value.charAt(0).toUpperCase() + value.slice(1)}</label>      
-            <input onClick={() => setParams({...params, [`${value}`]: true})} type="radio" name={value} />Yes
-            <input onClick={() => setParams({...params, [`${value}`]: false})} type="radio" name={value} />No
+        <label className='form-label me-3'>{value.charAt(0).toUpperCase() + value.slice(1)}</label>      
+            <input onClick={() => setParams({...params, [`${value}`]: true})} type="radio" name={value} className='form-check-input' /> Yes
+            <input onClick={() => setParams({...params, [`${value}`]: false})} type="radio" name={value} className='form-check-input ms-3' /> No
     </div>
   )
 }
