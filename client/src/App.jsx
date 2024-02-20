0e77c9a4cff847cf8a09c3cb637031594e611b2d
Maria Padilla <maria.pad.mar@gmail.com>
2024-02-20 12:43:25 +0100

    Draft tables database

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
+require("dotenv").config();
 import { useState } from 'react'
 
 import './App.css'
+
+const API_KEY = process.env.API_KEY
 
 function App() {
   
--- model/init_db.sql
+++ model/init_db.sql
@@ -3,16 +3,37 @@
 --
 
 SET foreign_key_checks = 0;
-DROP TABLE if exists students;
+DROP TABLE if exists plants;
+DROP TABLE if exists users;
 SET foreign_key_checks = 1;
 
 --
 -- Create Tables
 --
 
-CREATE TABLE students(
-    id INT NOT NULL AUTO_INCREMENT, 
-    firstname VARCHAR(40) not null, 
-    lastname VARCHAR(40) not null, 
+CREATE TABLE plants(
+    id INT NOT NULL AUTO_INCREMENT,
+    plant_api_id INT NOT NULL, 
+    scientific_name VARCHAR(100) NOT NULL, 
+    common_name VARCHAR(100),
+    photo VARCHAR(255), 
     PRIMARY KEY (id)
-    );
+);
+
+
+CREATE TABLE users(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(40) NOT NULL,
+    last_name VARCHAR(40) NOT NULL, 
+    email VARCHAR(255), 
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE users_plants(
+    id INT NOT NULL AUTO_INCREMENT,
+    plant_id INT NOT NULL, 
+    user_id INT NOT NULL,
+    FOREIGN KEY (plant_id) REFERENCES plants(id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
+    PRIMARY KEY (id)
+);
