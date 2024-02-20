2aadc088c381f7a7d735530d072a9e4b3fc32931
Maria Padilla <maria.pad.mar@gmail.com>
2024-02-20 12:57:36 +0100

    Add search form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,24 @@
-require("dotenv").config();
+// require("dotenv").config();
 import { useState } from 'react'
 
 import './App.css'
 
-const API_KEY = process.env.API_KEY
+// const API_KEY = process.env.API_KEY
 
 function App() {
   
   return (
     <>
-      This is my app
+      <h1>Find your favourite plants</h1>
+      <form>
+        <select>
+          <option>Filter by...</option>
+          <option>Scientific name</option>
+          <option>Common name</option>
+        </select>
+        <input placeholder="Write your query"></input>
+        <button>Search</button>
+      </form>
     </>
   )
 }
