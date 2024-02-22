1477cacb938de705ea5d018b1c866756431dc0ba
Maria Padilla <maria.pad.mar@gmail.com>
2024-02-22 13:34:11 +0100

    Style with icons PlantDetail

--- client/src/pages/PlantDetail.jsx
+++ client/src/pages/PlantDetail.jsx
@@ -1,6 +1,21 @@
 import { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import { apiClient } from "../apiClient";
+import {
+  Home,
+  Leaf,
+  HandHoldingSeeding,
+  Paw,
+  User,
+  Sun,
+  CalendarPen,
+  AppleWhole,
+  CloudSun,
+  Flower,
+  WaterBottle,
+  Tree,
+  Scissors,
+} from "react-flaticons";
 
 export default function PlantDetail() {
   const { id } = useParams();
@@ -47,40 +62,90 @@ export default function PlantDetail() {
   }
 
   return (
-    <>
-      <Link to="/catalog">Catalog</Link>
-      <div>PlantDetail {id}</div>
-      <div>
-        <h3>
-          <strong>{plant.common_name}</strong>
-        </h3>
-        <h5>
-          <em>{plant.scientific_name}</em>
-        </h5>
-        <img
-          src={
-            plant.default_image && plant.default_image.small_url
-              ? plant.default_image.small_url
-              : noImage
-          }
-          className="rounded mb-1"
-        />
-        <p>{plant.description}</p>
-        <p>Type: {plant.type}</p>
-        <p>Indoor: {plant.indoor === false ? "No" : "Yes"}</p>
-        <p>Poisonous to humans: {plant.poisonous_to_humans ? "Yes" : "No"}</p>
-        <p>Poisonous to pets: {plant.poisonous_to_pets ? "Yes" : "No"}</p>
-        <p>Edible leafs: {plant.edible_leaf ? "Yes" : "No"}</p>
-        <p>Edible fruits: {plant.edible_fruit ? "Yes" : "No"}</p>
-        <p>Flowers: {plant.flowers ? "Yes" : "No"}</p>
-        <p>Watering: {plant.watering}</p>
-        <p>Sunlight: {plant.sunlight}</p>
-        <p>
-          Pruning: {plant.pruning_month ? plant.pruning_month.join(", ") : null}
-        </p>
-        <p>
-          Propagation: {plant.propagation ? plant.propagation.join(", ") : null}
-        </p>
+    <div className="container">
+      <div className="row">
+        <Link to="/catalog">Catalog</Link>
+        <div className="col-4">
+          <img
+            src={
+              plant.default_image && plant.default_image.small_url
+                ? plant.default_image.small_url
+                : noImage
+            }
+            className="rounded mb-1 img-fluid"
+          />
+          <div>
+            <h3>
+              <strong>{plant.common_name}</strong>
+            </h3>
+            <h5>
+              <em>{plant.scientific_name}</em>
+            </h5>
+          </div>
+        </div>
+
+        <div className="col-8">
+          <div>
+            <p>{plant.description}</p>
+          </div>
+          <div className="row">
+            <div className="col-6">
+              <Tree />
+              <p>
+                <strong>Type:</strong> {plant.type}
+              </p>
+              <Home />
+              <p>
+                <strong>Indoor:</strong> {plant.indoor === false ? "No" : "Yes"}
+              </p>
+              <p>
+                <User />
+                <strong>Poisonous to humans:</strong>{" "}
+                {plant.poisonous_to_humans ? "Yes" : "No"}
+              </p>
+              <p>
+                <Paw />
+                <strong>Poisonous to pets:</strong>{" "}
+                {plant.poisonous_to_pets ? "Yes" : "No"}
+              </p>
+              <Leaf />
+              <p>
+                <strong>Edible leafs:</strong>{" "}
+                {plant.edible_leaf ? "Yes" : "No"}
+              </p>
+              <AppleWhole />
+              <p>
+                <strong>Edible fruits:</strong>{" "}
+                {plant.edible_fruit ? "Yes" : "No"}
+              </p>
+            </div>
+            <div className="col-6">
+              <Flower />
+              <p>
+                <strong>Flowers:</strong> {plant.flowers ? "Yes" : "No"}
+              </p>
+              <WaterBottle />
+              <p>
+                <strong>Watering:</strong> {plant.watering}
+              </p>
+              <Sun />
+              <CloudSun />
+              <p>
+                <strong>Sunlight:</strong> {plant.sunlight}
+              </p>
+              <Scissors /> <CalendarPen />
+              <p>
+                <strong>Pruning:</strong>{" "}
+                {plant.pruning_month ? plant.pruning_month.join(", ") : null}
+              </p>
+              <HandHoldingSeeding />
+              <p>
+                <strong>Propagation:</strong>{" "}
+                {plant.propagation ? plant.propagation.join(", ") : null}
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
 
       <div>
@@ -97,6 +162,6 @@ export default function PlantDetail() {
             ))
           : null}
       </div>
-    </>
+    </div>
   );
 }
