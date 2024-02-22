686c4f83951cd7cc3cf9f70af69d2176418f8c15
Maria Padilla <maria.pad.mar@gmail.com>
2024-02-22 16:08:27 +0100

    Create favorites page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
-import {Routes, Route} from "react-router-dom"
-import React from "react"
-import './App.css'
-import Home from "./pages/Home"
-import Catalog from "./pages/Catalog"
-import PlantDetail from "./pages/PlantDetail"
+import { Routes, Route } from "react-router-dom";
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import Catalog from "./pages/Catalog";
+import PlantDetail from "./pages/PlantDetail";
+import Favorites from "./pages/Favorites";
 
 // require("dotenv").config();
 // const API_KEY = import.meta.env.API_KEY
@@ -11,17 +12,14 @@ import PlantDetail from "./pages/PlantDetail"
 function App() {
   return (
     <>
-      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/catalog" element={<Catalog />} />
         <Route path="/catalog/:id" element={<PlantDetail />} />
-        
+        <Route path="/favorites" element={<Favorites />} />
       </Routes>
     </>
-    
-  )
- 
+  );
 }
 
-export default App
+export default App;
