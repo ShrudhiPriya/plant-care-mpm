f211c1f0a1d68b5f5c82a0b870082b314442934d
Maria Padilla <maria.pad.mar@gmail.com>
2024-02-23 18:14:13 +0100

    Disable pagination buttons when there are no more pages

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -3,10 +3,16 @@ import { ArrowAltRight, ArrowAltLeft } from "react-flaticons";
 export default function Pagination({ page, updatePageNumber }) {
   return (
     <div>
-      <button onClick={() => updatePageNumber(page.pageNumber - 1)}>
+      <button
+        onClick={() => updatePageNumber(page.pageNumber - 1)}
+        disabled={!page.hasPrevious}
+      >
         <ArrowAltLeft />
       </button>
-      <button onClick={() => updatePageNumber(page.pageNumber + 1)}>
+      <button
+        onClick={() => updatePageNumber(page.pageNumber + 1)}
+        disabled={!page.hasNext}
+      >
         <ArrowAltRight />
       </button>
       <div>{page.pageNumber}</div>
