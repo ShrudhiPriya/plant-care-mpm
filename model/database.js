f705485c4e01781b694b136d31046d633cd17b0c
Maria Padilla <maria.pad.mar@gmail.com>
2024-02-26 11:02:15 +0100

    Add instructions to create tables

--- model/database.js
+++ model/database.js
@@ -11,18 +11,20 @@ const con = mysql.createConnection({
   host: DB_HOST || "127.0.0.1",
   user: DB_USER || "root",
   password: DB_PASS,
-  database: DB_NAME || "facebook",
-  multipleStatements: true
+  database: DB_NAME || "plant_care",
+  multipleStatements: true,
 });
 
-con.connect(function(err) {
+con.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
 
   let sql = fs.readFileSync(__dirname + "/init_db.sql").toString();
-  con.query(sql, function(err, result) {
+  con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Table creation `students` was successful!");
+    console.log(
+      "Table creation `users, users_favourite_plants and plants` was successful!"
+    );
 
     console.log("Closing...");
   });
--- model/init_db.sql
+++ model/init_db.sql
@@ -1,39 +1,57 @@
 --
 -- Drop Tables
 --
+SET
+    foreign_key_checks = 0;
 
-SET foreign_key_checks = 0;
 DROP TABLE if exists plants;
+
 DROP TABLE if exists users;
-SET foreign_key_checks = 1;
+
+DROP TABLE if exists users_favourite_plants;
+
+SET
+    foreign_key_checks = 1;
 
 --
 -- Create Tables
 --
+CREATE TABLE users(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
+);
 
 CREATE TABLE plants(
-    id INT NOT NULL AUTO_INCREMENT,
-    plant_api_id INT NOT NULL, 
-    scientific_name VARCHAR(100) NOT NULL, 
-    common_name VARCHAR(100),
-    photo VARCHAR(255), 
-    PRIMARY KEY (id)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    plant_api_id BIGINT NOT NULL,
+    scientific_name VARCHAR(255) NOT NULL,
+    common_name VARCHAR(255) NOT NULL,
+    thumbnail VARCHAR(255),
+    medium_image VARCHAR(255),
+    plant_type VARCHAR(255),
+    indoor TINYINT(1),
+    poisonous_to_humans TINYINT(1),
+    poisonous_to_pets TINYINT(1),
+    plant_description VARCHAR(1000),
+    edible_fruit TINYINT(1),
+    edible_leaf TINYINT(1),
+    flowers TINYINT(1),
+    cycle VARCHAR(255),
+    watering VARCHAR(255),
+    propagation VARCHAR(255),
+    sunlight VARCHAR(255),
+    pruning_month VARCHAR(255),
+    watering_description VARCHAR(1000),
+    sunlight_description VARCHAR(1000),
+    pruning_description VARCHAR(1000)
 );
 
-
-CREATE TABLE users(
-    id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(40) NOT NULL,
-    last_name VARCHAR(40) NOT NULL, 
-    email VARCHAR(255), 
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE users_plants(
-    id INT NOT NULL AUTO_INCREMENT,
-    plant_id INT NOT NULL, 
+CREATE TABLE users_favourite_plants(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
-    FOREIGN KEY (plant_id) REFERENCES plants(id)
-    FOREIGN KEY (user_id) REFERENCES users(id)
-    PRIMARY KEY (id)
+    plant_id INT NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(plant_id) REFERENCES plants(id)
 );
